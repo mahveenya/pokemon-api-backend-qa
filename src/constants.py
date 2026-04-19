bf07e04e43bc68373a49995ed210c711b4fc892a
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2026-04-19 23:35:28 +0300

    feat: add versioning to api

--- src/constants.py
+++ src/constants.py
@@ -1,8 +1,8 @@
 class Endpoints:
-    ROOT = "/"
-    POKEMON = "/pokemon"
-    POKEMON_BY_ID_OR_NAME = "/pokemon/{id_or_name}"
-    ABILITY_BY_ID = "/ability/{id_or_name}"
+    ROOT = "api/v1/"
+    POKEMON = "api/v1/pokemon"
+    POKEMON_BY_ID_OR_NAME = "api/v1/pokemon/{id_or_name}"
+    ABILITY_BY_ID = "api/v1/ability/{id_or_name}"
 
 
 class DefaultValues:
